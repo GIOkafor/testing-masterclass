3c27c5552169b65dc9e55c82b36860b9d403e71b
iyk3 <ikechukwuokafor@hotmail.co.uk>
2022-11-19 17:35:06 -0500

    include env file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true, 
+    firebase: {
+        apiKey: "AIzaSyDb2AQUTHIuB33Ub-B5Z8UUWBdcX1cgcCQ",
+        authDomain: "meal-logger.firebaseapp.com",
+        databaseURL: "https://meal-logger.firebaseio.com",
+        projectId: "meal-logger",
+        storageBucket: "meal-logger.appspot.com",
+        messagingSenderId: "829653545401",
+        appId: "1:829653545401:web:1d05b50c75e4a0bfd70668"
+    }
+};
